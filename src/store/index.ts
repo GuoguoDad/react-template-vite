2c9f8a859a6413f49ac9e887f6e8f0d66f2e70d1
刘辉20020013 <liuhui@LiuHuis-MacBook-Pro.local>
2025-05-13 16:17:33 +0800

    feat: update deps

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,9 @@
 import logger from 'redux-logger'
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit'
 import rootReducer from './rootReducer'
-
-const middleware = getDefaultMiddleware().concat(logger)
 
 export const store = configureStore({
   reducer: rootReducer,
-  middleware,
   devTools: process.env.NODE_ENV !== 'production'
 })
 
